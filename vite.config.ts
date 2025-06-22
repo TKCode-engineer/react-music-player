7391842b67e454cbb316b60cf6f47c7934e883fd
taka <taka198728@gmail.com>
2025-06-22 09:04:21 +0900

    初回コミット

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+});
